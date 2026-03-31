12bd476516cf1376e97b284217ecb85512a247f9
Raju communication <269435034+rajucommunication1120-prog@users.noreply.github.com>
2026-03-31 13:37:04 +0530

    Add form for creating a distributor

---
+++ add_distributor.php
@@ -0,0 +1,6 @@
+<form method="POST" action="save_distributor.php">
+  Name: <input type="text" name="name"><br>
+  Mobile: <input type="text" name="mobile"><br>
+  Password: <input type="password" name="password"><br>
+  <button type="submit">Create Distributor</button>
+</form>
